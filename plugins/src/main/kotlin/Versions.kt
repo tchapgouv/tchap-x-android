db8ea9719be3c9de6a72bff4862ad29b24b3b309
Raphaël Chevallier <raphael.chevallier.ext@numerique.gouv.fr>
2026-04-02 14:31:26 +0200

    Version 0.8.0

--- plugins/src/main/kotlin/Versions.kt
+++ plugins/src/main/kotlin/Versions.kt
@@ -50,7 +50,7 @@ private const val versionReleaseNumber = 3
 
 // TCHAP - rework version number
 private const val versionMajorNumber = 0
-private const val versionMinorNumber = 7
+private const val versionMinorNumber = 8
 private const val versionPatchNumber = 0
 
 object Versions {
